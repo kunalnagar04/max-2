bffdd4737755bba44f7769dbc9a1121a8e9d2e6d
Akash Sharma <89353868+AkashFlix@users.noreply.github.com>
2021-10-12 15:49:40 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -5,12 +5,12 @@
 class Translation(object):
     
     START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+<i>Am Just A Advance Auto Filter Bot 😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+For use me Just write only Movies/Series name in my group @RequestHubFlix 😏
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+For Request Movies/Series Join my Group ...
+@RequestHubFlix 💞
 </i>"""    
     
     HELP_TEXT = """
@@ -46,13 +46,13 @@ For More Details Click Help Button Below..
 @CrazyBotsz
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>Ⲕⲓʀⲁ Ϝⳑⲓⲭ</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Bot_Flix">Bot_Flix</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/MoviesHubFlix">MoviesHubFlix</a></i>
 """
