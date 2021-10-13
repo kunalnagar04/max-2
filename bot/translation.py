4a95d3134e378f3f82d04f74902edafa70d0c10a
Akash Sharma <89353868+AkashFlix@users.noreply.github.com>
2021-10-13 12:29:38 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -8,43 +8,22 @@ class Translation(object):
     
 <b>Hᴇʟʟᴏ sɪʀ I'ᴍ ᴀ Kɪʀᴀ 😎
 
- I ᴄᴀɴ Pʀᴏᴠɪᴅᴇ ᴀɴʏ Mᴏᴠɪᴇ ᴏʀ Sᴇʀɪᴇs ʏᴏᴜ ᴡᴀɴᴛ...Jᴜsᴛ Jᴏɪɴ Mʏ Gʀᴏᴜᴘ ᴀɴᴅ
+ I ᴄᴀɴ Pʀᴏᴠɪᴅᴇ ᴀɴʏ Mᴏᴠɪᴇ ᴏʀ Sᴇʀɪᴇs ʏᴏᴜ ᴡᴀɴᴛ...Jᴜsᴛ Jᴏɪɴ Mʏ Gʀᴏᴜᴘ [ @RequestHubFlix ] ᴀɴᴅ
 Wʀɪᴛᴇ Mᴏᴠɪᴇs Nᴀᴍᴇ Oɴʟʏ
 
 🔅 <b>@MoviesHubFlix</b> 🔅
 </b>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>How To Contact Us!?</u></i></b>
 
-<i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
-</i>
+⚡ <b>Here the Admins</b> ⚡
+ 
+🔸 <b>@Bot_Flix</b>
+🔹 <b>@Lord_Kira1</b>
+🔸 <b>@Anime_Hub_Flix</b>
 
-<b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-     
-    -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-     
-    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-    
-    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
-
-            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
-            
-            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
-
-            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
-            
-<b>@MoviesHubFlix</b>
+<b>🔅 @MoviesHubFlix 🔅</b>
 """
     
     ABOUT_TEXT = """<b>🔸 Name</b> : <b>Ⲕⲓʀⲁ Ϝⳑⲓⲭ</b>
@@ -52,8 +31,6 @@ Wʀɪᴛᴇ Mᴏᴠɪᴇs Nᴀᴍᴇ Oɴʟʏ
 <b>🔹 Developer</b> : <b><i><a href="https://t.me/Bot_Flix">Bot_Flix</a></i></b>
 
 <b>🔸 Language</b> : <code>Python3</code>
-
-<b>🔹 Source</b> : <b><a href="https://t.me/Bot_Flix">Bot_Flix </a></b>
 
 <b>🔸 Channel</b> : <i><a href="https://t.me/MoviesHubFlix">MoviesHubFlix</a></b>
 """
